0d940375d608b100df3f62f19542c188f81835fe
Bill Wang <bill.wang>
2019-09-11 22:45:03 +1000

    Add travis pipeline

--- build.sh
+++ build.sh
@@ -10,8 +10,8 @@ version=${2}
 image="alpine/greengrass:${version}-${arch}"
 
 # install latest docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 case ${arch} in
